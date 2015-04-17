e984859c279050922069c7f00161e76e9b2be8cc
stygian-phrygian <no@no.no>
2015-04-17 02:06:50 -0400

    added developer tools macro to _chrome.py

--- _chrome.py
+++ _chrome.py
@@ -32,7 +32,9 @@ chrome_rule = MappingRule(
         "zoom out [<n>] [(time|times)]"      : Key("c-minus:%(n)d"),
         "zoom in  [<n>] [(time|times)]"      : Key("c-plus:%(n)d"),
         "refresh"                            : Key("c-r"),
-        "view source"                        : Key("c-u")
+        "view source"                        : Key("c-u"),
+        "(show|toggle) developer [tools]"    : Key("cs-i"),
+        "(show|toggle) javascript console"   : Key("cs-j"),
     },
     extras=[
         Integer("n", 1, 40),
